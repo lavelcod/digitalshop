cd8d393157b300d4e4d2b6f5dbbdecadd2165b3d
lavelcod <lavelcod@gmail.com>
2025-09-21 19:36:04 +0330

    shadcn installation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
-  console.log('test')
+  console.log('test');
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       my first page
