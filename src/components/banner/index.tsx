c53f8574ce5ca20d9a6b17e2e7c5f1b461989558
lavelcod <lavelcod@gmail.com>
2025-09-22 07:04:00 +0330

    fixing prettier and esling by 'https://chatgpt.com/share/68d0c397-308c-800a-9b2d-db0062c91dcc'

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,42 +1,46 @@
 import * as React from 'react';
 
 import {
-    Card,
-    CardContent,
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
+  Card,
+  CardContent,
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
 } from '@/components/ui';
 
-import IMG1 from './image/1.webp'
-import IMG2 from './image/2.png'
-import IMG3 from './image/3.jpg'
-import IMG4 from './image/4.webp'
+import IMG1 from './image/1.webp';
+import IMG2 from './image/2.png';
+import IMG3 from './image/3.jpg';
+import IMG4 from './image/4.webp';
 import Image from 'next/image';
 
 function Banner() {
-    return (
-        <Carousel className="w-full">
-            <CarouselContent>
-                {[IMG1, IMG2, IMG3, IMG4].map((image, index) => (
-                    <CarouselItem key={index}>
-                        <div className="p-1">
-                            <Card>
-                                <CardContent className="relative flex items-center justify-center aspect-square h-[400px] w-full p-0">
-                                    <Image src={image} alt='banner' fill className='object-cover'/>
-                                </CardContent>
-                            </Card>
-                        </div>
-                    </CarouselItem>
-                ))}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-        </Carousel>
-    );
+  return (
+    <Carousel className="w-full">
+      <CarouselContent>
+        {[IMG1, IMG2, IMG3, IMG4].map((image, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <Card>
+                <CardContent className="relative flex items-center justify-center aspect-square h-[400px] w-full p-0">
+                  <Image
+                    src={image}
+                    alt="banner"
+                    fill
+                    className="object-cover"
+                  />
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
 }
-
 
 export default Banner;
