6f12ceae6d552322c4a2a0ef7c8812a367e4e4c6
lavelcod <lavelcod@gmail.com>
2025-09-22 05:53:51 +0330

    npx shadcn@latest add card

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,13 +1,13 @@
-import * as React from "react"
+import * as React from 'react';
 
-import { Card, CardContent } from "@/components/ui/card"
+import { Card, CardContent } from '@/components/ui/card';
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel"
+} from '@/components/ui/carousel';
 
 export function Banner() {
   return (
@@ -28,5 +28,5 @@ export function Banner() {
       <CarouselPrevious />
       <CarouselNext />
     </Carousel>
-  )
+  );
 }
